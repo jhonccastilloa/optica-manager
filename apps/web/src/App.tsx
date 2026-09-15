ab80f3c7ff8bd10cda47010e37c2e03f0e292e09
Jhon Castillo <gpro1pro@gmail.com>
2026-09-15 18:20:34 -0500

    feat: add global toast notifications

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router/dom"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toast"
 import { router } from "@/router/router"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="optica-manager-theme">
       <RouterProvider router={router} />
+      <Toaster />
     </ThemeProvider>
   )
 }
